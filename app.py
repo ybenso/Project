d0ccb81b3800b8ceba743f4014f7eba8fa53ad27
Younes BENSOUDA <ybensouda@equancy.com>
2021-11-23 15:09:07 -0500

    add a dot

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import datetime
 
 ticker = st.text_input('Stock Ticker', 'JNJ')
 stock_data = get_stock_data(ticker)
-st.write('The current stock modeled is', stock_data.info['longName'])
+st.write('The current stock modeled is', stock_data.info['longName'],".")
 
 start = st.date_input("Start date",datetime.date(1980, 1, 1))
 st.write('The start date is:', start)
